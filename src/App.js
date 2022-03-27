018411f1219a8926937067be77823dcd082f1005
Sergio D'Elia <sergio.delia@yahoo.it>
2022-03-27 22:28:52 +0200

    Added all players mode

--- src/App.js
+++ src/App.js
@@ -17,13 +17,60 @@ const [record, setRecord] = useState(0);
 const [cambio, setCambio] = useState(false);
 const [calciatore, setCalciatore] = useState(null);
 const [calciatore2, setCalciatore2] = useState(null);
+const [aggiorna, setAggiorna] = useState(-1);
 const[maggiore, setMaggiore] = useState(true);
 const url = 'https://transfermarket.p.rapidapi.com/clubs/get-squad';
+
+
+const getAllData = async () => {
+  
+  datiSquadra.length > 0 && setDatiSquadra([]);
+  let random = Math.floor(Math.random() * squadre.length);
+  const { data } = await axios.get(url, {
+    method: 'GET',
+    params: {id: squadre[random]},
+    headers: {
+      'X-RapidAPI-Host': 'transfermarket.p.rapidapi.com',
+      'X-RapidAPI-Key': '4f817f4bb6msh6e032e354e1ae92p1aa27djsncff60d5f250c',
+      'content-type': 'application/json'
+    }
+  });
+  if(aggiorna == -1){
+    setAggiorna(0);
+  }
+  setDati(data.squad);
+
+}
+
+
+useEffect(() => {
+  
+  if(dati){
+    let random = Math.floor(Math.random() * dati.length);
+    let random2 = Math.floor(Math.random() * dati.length);
+    switch(aggiorna){
+      case 1:
+        setCalciatore(dati[random]);
+        break;
+      case 2:
+        setCalciatore2(dati[random2]);
+        break;
+      default:
+        setCalciatore(dati[random]);
+        setCalciatore2(dati[random2]);
+        break;
+    } 
+   }
+  setCambio(!cambio);
+}, [dati])
+
+
 
 
 
 const getData = async (id) => {
 
+  (dati && setDati(null));
   const { data } = await axios.get(url, {
     method: 'GET',
     params: {id: id},
@@ -40,13 +87,15 @@ const getData = async (id) => {
 
 useEffect(() => {
 
-  console.log('Use Effect datiSquadra');
-  console.log(datiSquadra);
-  const random = Math.floor(Math.random() * datiSquadra.length);
-  setCalciatore(datiSquadra[random]);
-  const random2 = Math.floor(Math.random() * datiSquadra.length);
-  setCalciatore2(datiSquadra[random2]);
-  setCambio((cambio) => !cambio);
+  if(datiSquadra.length > 0){
+    console.log('Use Effect datiSquadra');
+    console.log(datiSquadra);
+    const random = Math.floor(Math.random() * datiSquadra.length);
+    setCalciatore(datiSquadra[random]);
+    const random2 = Math.floor(Math.random() * datiSquadra.length);
+    setCalciatore2(datiSquadra[random2]);
+    setCambio((cambio) => !cambio);
+  }
 
 }, [datiSquadra]);
 
@@ -81,6 +130,17 @@ useEffect(() => {
 
 
 
+useEffect(() => {
+
+  if(aggiorna != -1 && aggiorna != 0){
+    getAllData();
+  }
+
+}, [aggiorna])
+
+
+
+
 
 const ricomincia = async () => {
 
@@ -96,7 +156,8 @@ const ricomincia = async () => {
 }
 
 const handleSubmit = (giusto, posizione) => {
-  if(!labelWin.corretta){
+
+if(aggiorna == -1){  if(!labelWin.corretta){
     setLabelWin({...labelWin, messaggio: 'Ricomincia per giocare!'});
     return;
   }
@@ -126,11 +187,42 @@ const handleSubmit = (giusto, posizione) => {
   }
   setMaggiore(!maggiore);
   setCambio(!cambio);
+} else {
+  //SE GIOCHIAMO CON TUTTE LE SQUADRE
+  if(!labelWin.corretta){
+    setLabelWin({...labelWin, messaggio: 'Ricomincia per giocare!'});
+    return;
+  }
+  if(giusto){
+    if(record < labelWin.giuste+1){
+      setRecord(record+1);
+    }
+    setLabelWin((prev) => ({
+      messaggio: "Risposta corretta!",
+      corretta: true,
+      giuste: prev.giuste + 1,
+    }));
+    const random = Math.floor(Math.random() * datiSquadra.length);
+    if(posizione == 1){
+      setAggiorna(1);
+    } else {
+      setAggiorna(2);
+    }
+  } else {
+
+    setLabelWin((prev) => ({
+      messaggio: "Risposta errata!",
+      corretta: false,
+      giuste: prev.giuste,
+    }))
+    
+  }
+  setMaggiore(!maggiore);
+  setCambio(!cambio);
 
 }
+}
 
-
-console.log(squadra);
 
   return (
     <div className="App">
@@ -157,11 +249,12 @@ console.log(squadra);
 <option value="380">Salernitana</option>
 <option value="607">Venezia</option>
 </select>
-      <button onClick={() => getData(squadra)}>INIZIA A GIOCARE</button>
-
-{(datiSquadra.length>20) && (
+      <button onClick={() => getData(squadra)}>GIOCA CON UNA SOLA SQUADRA</button>
+      {(datiSquadra.length>20) && (
       <button onClick={() => ricomincia()}>RICOMINCIA</button>
 )}
+<button onClick={() => getAllData()}>GIOCA CON TUTTE</button>
+
       {(calciatore && calciatore2) ? (
         <div>
          
@@ -174,17 +267,17 @@ console.log(squadra);
           <div className='col-md-6'> 
           <h2>{calciatore.name}</h2>
           <img className={labelWin.corretta && (calciatore.giusto ? 'corretta': 'sbagliata')}  src={calciatore.heroImage ?? calciatore.image} onClick={()=> handleSubmit(calciatore.giusto,1)} />
-          {(1==2) && (
+         
           <h5>{calciatore.giusto ? 'Giusto' : 'Non giusto'} - {calciatore.marketValue.value}</h5>
-          )}
+         
         </div>
         <div className='col-md-6'> 
         <hr className='divisore'/> 
           <h2>{calciatore2.name}</h2>
           <img className={labelWin.corretta && (calciatore2.giusto ? 'corretta': 'sbagliata')} src={calciatore2.heroImage ?? calciatore2.image} onClick={() => handleSubmit(calciatore2.giusto,2)} />
-          {(1==2) && (
+  
           <h5>{calciatore2.giusto ? 'Giusto' : 'Non giusto'} - {calciatore2.marketValue.value}</h5>
-          )}
+  
           </div>
         </div>
         </div>
